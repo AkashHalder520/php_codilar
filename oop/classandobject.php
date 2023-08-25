1aed38b7b7914eec3cecf0c17e5509caaed1f847
AkashHalder520 <akashhalder520@gmail.com>
2023-08-25 21:42:43 +0530

    oop

---
+++ oop/classandobject.php
@@ -0,0 +1,20 @@
+<?php
+class class1{
+   public $var="akash";// we have to defne if its public or private
+//if used private can not used or called ouside the class 
+ // by default public
+function func1(){
+        echo "function one <br>";
+        $this->var="ramesh";
+    }
+    function func2(){
+        echo "function two";
+    }
+
+}
+$obj =new class1();
+$obj2 =new class1();
+$obj->func1();
+echo $obj->var."this is object 1<br>";//we dont have to use $ for accessing variable from class
+echo $obj2->var."this is object 2 <br>";//
+?>
